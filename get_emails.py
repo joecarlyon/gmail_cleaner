51a6680d04da218fefff4cfde786b50a7f776d5a
Joseph Carlyon <joecarlyon@gmail.com>
2018-11-26 21:28:54 -0600

    Creating testable methods

--- get_emails.py
+++ get_emails.py
@@ -5,29 +5,13 @@ from oauth2client import file, client, tools
 from get_attachments import get_attachments
 import json
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+# Requires access to delete accounts. If you don't want that, instead use:
+# https://www.googleapis.com/auth/gmail.readonly
+SCOPES = 'https://mail.google.com/'
 
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('gmail', 'v1', http=creds.authorize(Http()))
-
-    # Call the Gmail API
-    response = service.users().messages().list(userId='me', q="has:attachment", maxResults=500).execute()
-    messages = response['messages']
+def download_attachments_from_messages(service, messages):
     messages_with_attachments = list()
-
     if not messages:
         print('No messages to handle.')
     else:
@@ -38,10 +22,28 @@ def main():
             if msg_id:
                 messages_with_attachments.append(msg_id)
 
-    print(f"{len(messages_with_attachments)} with attachments were downloaded.")
+    return messages_with_attachments
 
-    response = service.users().messages().batchDelete(userId='me', body=json.dumps(messages_with_attachments)).execute()
-    response
+
+def main():
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+    # Find messages with attachments and download them
+    response = service.users().messages().list(userId='me', q="has:attachment", maxResults=500).execute()
+    messages = response['messages']
+    messages_with_attachments = download_attachments_from_messages(service, messages)
+
+    # Delete the messages
+    print(f"{len(messages_with_attachments)} with attachments were downloaded.")
+    service.users().messages().batchDelete(userId='me', body=json.dumps(messages_with_attachments)).execute()
 
 
 if __name__ == '__main__':
